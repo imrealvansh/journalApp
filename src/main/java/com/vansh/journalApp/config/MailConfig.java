c078a57d90fe74be2885b06d026b40353fe2d3d4
imrealvansh <115864268+imrealvansh@users.noreply.github.com>
2024-10-31 10:18:43 +0530

    mail config file

---
+++ src/main/java/com/vansh/journalApp/config/MailConfig.java
@@ -0,0 +1,30 @@
+package com.vansh.journalApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.google.com");  // Replace with your SMTP host
+        mailSender.setPort(587);                   // Replace with your SMTP port
+
+        mailSender.setUsername("imrealvansh@gmail.com");  // Replace with your email
+        mailSender.setPassword("hmpp obge cojg whrq");      // Replace with your password
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
